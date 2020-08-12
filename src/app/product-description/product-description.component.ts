43c32e48477d769b987ef11e6e70138acb76c0c4
max_zamarripa <max.zamarripa@softtek.com>
2020-08-11 18:33:34 -0700

    Part3 Remove Console Log

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -13,10 +13,7 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(
-      response => {this.albumInfo = response; console.log(response);} 
-    );
-    
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
   
 }
